59e37d1b731092ee7b0a908f6aaa4a9682f3454c
taigai.lzy <taigai.lzy@antfin.com>
2021-12-22 17:04:27 +0800

    add traceRunnable

---
+++ src/main/java/cc/lzy/spring/consurrent/ClassUtils.java
@@ -0,0 +1,10 @@
+package cc.lzy.spring.consurrent;
+
+/**
+ * class function desc
+ *
+ * @author taigai
+ * @date 2021/12/22 5:01 PM
+ */
+public class ClassUtil {
+}
---
+++ src/main/java/cc/lzy/spring/consurrent/TraceThreadPoolExecutor.java
@@ -0,0 +1,10 @@
+package cc.lzy.spring.consurrent;
+
+/**
+ * class function desc
+ *
+ * @author taigai
+ * @date 2021/12/22 4:57 PM
+ */
+public class TraceRunable {
+}
